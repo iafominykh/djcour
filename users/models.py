6dec3e3fe79967ec63d26432eb79e08be24acfcd
iafominykh <iafominykh@yandex.ru>
2023-07-27 16:58:32 +0300

    2 commit

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+NULLABLE = {'null': True, 'blank': True}
+
+
 # Create your models here.
+class User(models.Model):
+    email = models.CharField(max_length=250, verbose_name='Адрес электронной почты')
+    surname = models.CharField(max_length=200, verbose_name='Фамилия')
+    name = models.CharField(max_length=200, verbose_name='Имя')
+    patronymic = models.CharField(**NULLABLE, max_length=200, verbose_name='Отчество')
+    comment = models.TextField(**NULLABLE, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
---
+++ users/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+
+from users.apps import UsersConfig
+
+app_name = UsersConfig.name
+
